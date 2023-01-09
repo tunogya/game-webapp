de8985df77fd730be2c73710ca0d206c87ca14c4
tunogya <tunogya@qq.com>
2023-01-09 17:52:10 +0800

    add bot

---
+++ lambda/bot.js
@@ -0,0 +1,40 @@
+const {Telegraf, Markup} = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  try {
+    ctx.reply("Welcome!")
+  } catch (e) {
+    console.log(e)
+  }
+});
+
+bot.command("dune", (ctx) => {
+  try {
+    ctx.replyWithGame('dune', Markup.inlineKeyboard([
+      Markup.button.game("Play Solo"),
+      Markup.button.url("Github", "https://github.com/tunogya/arrakis-dune")
+    ]))
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+bot.gameQuery((ctx) => {
+  try {
+    const game_short_name = ctx.update.callback_query.game_short_name;
+    ctx.answerGameQuery(`https://${game_short_name}.game.wizardingpay.com`)
+  } catch (e) {
+    console.log(e)
+  }
+});
+
+exports.handler = async (event, context, callback) => {
+  const tmp = JSON.parse(event.body);
+  await bot.handleUpdate(tmp);
+  return callback(null, {
+    statusCode: 200,
+    body: '',
+  });
+};
