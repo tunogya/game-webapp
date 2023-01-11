a81130ca7ff4f2088a84fa82bc667fe5db07d322
tunogya <tunogya@qq.com>
2023-01-11 16:51:55 +0800

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,17 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <Center>
         <Component {...pageProps} />
-        <Script src="https://telegram.org/js/games.js"/>
+        <Script src={"https://telegram.org/js/games.js"}/>
+        <Script src={"https://www.googletagmanager.com/gtag/js?id=G-GY77H61X8Q"}></Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                
+                gtag('config', 'G-GY77H61X8Q');
+              `}
+        </Script>
       </Center>
     </ChakraProvider>
   )
