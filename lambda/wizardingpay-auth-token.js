2c831c7b3aa31e77302175faff11010befc4838f
tunogya <tunogya@qq.com>
2023-01-15 01:50:40 +0800

    update

--- lambda/wizardingpay-auth-token.js
+++ lambda/wizardingpay-auth-token.js
@@ -58,9 +58,6 @@ exports.handler = async (event) => {
     
       return {
         statusCode: 200,
-        headers: {
-          "Content-Type": "text/plain",
-        },
         body: token,
       }
     }
