c5dcc2564c2e7f6b334675b64f3741c5fae3f362
tunogya <tunogya@qq.com>
2023-01-16 22:42:50 +0800

    add telegram webapp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,6 +69,7 @@ export default function App({Component, pageProps}: AppProps) {
             <Center>
               <Component {...pageProps} />
               <Script src={"https://telegram.org/js/games.js"}/>
+              <Script id={"telegram-web-app"} src={"https://telegram.org/js/telegram-web-app.js"}/>
               <Script src={"https://www.googletagmanager.com/gtag/js?id=G-GY77H61X8Q"}></Script>
               <Script id="google-tag-manager" strategy="afterInteractive">
                 {`
