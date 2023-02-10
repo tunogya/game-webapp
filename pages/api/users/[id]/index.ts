72ad95e45d867a3f3b559e4e50bbf03cea45719f
tunogya <tunogya@qq.com>
2023-02-11 00:39:35 +0800

    update

--- pages/api/users/[id]/index.ts
+++ pages/api/users/[id]/index.ts
@@ -5,6 +5,10 @@ import {NextApiRequest, NextApiResponse} from 'next';
 
 const ddbClient = new DynamoDBClient({
   region: 'ap-northeast-1',
+  credentials: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
+  }
 });
 
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, {
